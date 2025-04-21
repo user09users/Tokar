904debaed2f556c5e5807742672303b2d1c5e6f8
Mkh <example@gmail.com>
2025-04-21 12:21:43 +0200

    HTML_cardPage_slider

--- src/js/main.js
+++ src/js/main.js
@@ -2,16 +2,18 @@ import "purecss/build/grids-min.css";
 import "purecss/build/grids-responsive-min.css";
 
 import hamburger from "./hamburger";
-import slider from "./slider";
+import { sliders } from "./slider";
 import filters from "./filters";
 import sliderRange from "./sliderRange";
 import search from "./search";
+import cardPageslider from "./cardPageSlider";
 
 hamburger();
-slider();
+sliders();
 filters();
 sliderRange();
 search();
+cardPageslider();
 
 import "/src/sass/style.scss";
 
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,11 +1,12 @@
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
-
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-const slider = () => {
+let mainSlider;
+
+const sliders = () => {
     try {
         new Swiper('.materials__slider', {
             slidesPerView: 1,
@@ -19,8 +20,8 @@ const slider = () => {
                 prevEl: ".icon-left-big",
             },
             modules: [Navigation, Pagination],
-
         });
+
         new Swiper('.lastStates__slider', {
             slidesPerView: 1,
             loop: true,
@@ -46,8 +47,8 @@ const slider = () => {
                 },
             },
             modules: [Navigation, Pagination],
-
         });
+
         new Swiper('.ceo__slider', {
             slidesPerView: 1,
             loop: true,
@@ -60,9 +61,26 @@ const slider = () => {
                 prevEl: ".icon-left-big",
             },
             modules: [Navigation, Pagination],
-
         });
-    } catch (e) { }
+
+        // Export the mainSlider instance for external sync
+        mainSlider = new Swiper('.cardSlider__slider', {
+            slidesPerView: 1,
+            loop: true,
+            pagination: {
+                el: ".cardSlider__slider-nav",
+                clickable: true,
+            },
+            navigation: {
+                nextEl: ".icon-right-big",
+                prevEl: ".icon-left-big",
+            },
+            modules: [Navigation, Pagination],
+        });
+
+    } catch (e) {
+        console.error('Swiper initialization failed:', e);
+    }
 };
 
-export default slider;
+export { sliders, mainSlider };
