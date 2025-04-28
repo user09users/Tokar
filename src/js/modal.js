6648b096e412eeb8b26b33cfb01809cc75fbfb5f
Mkh <example@gmail.com>
2025-04-28 21:14:54 +0200

    HTML_improvement

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,24 +1,38 @@
 const modal = () => {
 
-    const openBtn = document.querySelectorAll('.button');
+    const openBtn = document.querySelectorAll('[data-openModal');
     const closeBtn = document.querySelector('[data-close]');
     const modal = document.querySelector('[data-consultation]');
     const overlay = document.querySelector('.modal__overlay');
 
-    openBtn.forEach(btn => {
-        btn.addEventListener('click', () => {
-            modal.classList.add('active');
-            document.body.style.overflow = 'hidden'; // Disable page scroll
-        });
-    })
+    const openModal = () => {
+        modal.classList.add('active');
+        document.body.style.overflow = 'hidden';
+    };
 
     const closeModal = () => {
         modal.classList.remove('active');
         document.body.style.overflow = ''; // Restore scroll
     };
 
+    openBtn.forEach(btn => {
+        btn.addEventListener('click', openModal);
+    });
+
     closeBtn.addEventListener('click', closeModal);
     overlay.addEventListener('click', closeModal);
+
+
+
+    const showModalByScroll = () => {
+        // Check if the user has scrolled to the bottom of the page
+        if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    };
+
+    window.addEventListener('scroll', showModalByScroll);
 }
 
 export default modal;
